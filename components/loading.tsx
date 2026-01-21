154eee21968b72edba75b6f4591620bcbee13297
Samarth <subedi42samarth@gmail.com>
2026-01-21 10:19:27 +0545

    fix(loading): adjust loading component height to full for better display

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center">
       <Loader2 className=" h-12 w-12 text-primary left-1/2 animate-spin" />
     </div>
   );
